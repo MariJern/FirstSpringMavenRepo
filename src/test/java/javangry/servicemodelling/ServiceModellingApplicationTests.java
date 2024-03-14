73de18495a962d76952c717fbff9fd7f257028e0
MariJern <marina.jtest@gmail.com>
2024-03-14 11:19:22 -0500

    first commit

---
+++ src/test/java/javangry/servicemodelling/ServiceModellingApplicationTests.java
@@ -0,0 +1,13 @@
+package javangry.servicemodelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceModellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
